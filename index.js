24a3b22f485c461d3e2f33838184be19d95e4a19
Mustafa <hazim6163@gmail.com>
2019-10-30 06:43:40 +0100

    init project

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const upload = require('express-fileupload');
+
+// connect to the database :
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_HOST,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+// check if the we connected to db or not 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('connected to the mongo db !!');
+});
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(upload());
+
+app.listen(3001, () => {
+    console.log("Server working on Port: 3000");
+});
